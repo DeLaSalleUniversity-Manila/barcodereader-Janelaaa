68feb5d5d90a6ef8068a37c653657afc4998e8f0
Janela Angeles <janela_angeles@dlsu.edu.ph>
2015-12-08 12:11:20 +0800

    Initial COmmit

---
+++ app/src/main/java/angeles/janela/assumpta/barcodescandemo/AnyOrientationActivity.java
@@ -0,0 +1,11 @@
+package angeles.janela.assumpta.barcodescandemo;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class AnyOrientationActivity extends CaptureActivity {
+
+}
